f675493cc496a0bc4a1841868fa1f90f10e96ca6
Jesse Roe <roeje@arch07.cis.gvsu.edu>
2016-04-07 10:04:07 -0400

    added digitize and chepast date functions

--- pos.hs
+++ pos.hs
@@ -1,6 +1,24 @@
-taxRate :: Foat
-let taxRate = 0.065
+import Data.Time.Calendar
+
+taxRate :: Float
+taxRate = 0.065
 
 digitize :: Integer -> [Integer]
-digitize x = digitize (x 'div' 10) ++ [x 'mod' 10]
+digitize 0 = []
+digitize x = digitize (x `div` 10) ++ [x `mod` 10]
 
+cardType :: Integer -> String
+cardType x | head (digitize x) == 4 = "Visa"
+		   | head (digitize x) == 5 = "MasterCard"
+		   | head (digitize x) == 6 = "Discover"
+		   | otherwise = "Not Accepted"
+
+pastDate :: Integer -> Int -> Int -> IO Bool
+pastDate x y z
+
+
+
+getDate :: IO (Integer, Int, Int)
+getDate = getCurrentDate
+
+return IO Bool
