395ec407be0daec7cb5fa18eae8d36520837d9a2
Jesse Roe <roeje@arch07.cis.gvsu.edu>
2016-04-15 13:18:11 -0400

    added extra functions and fixed whitespace error in checkOut

--- pos.hs
+++ pos.hs
@@ -1,5 +1,6 @@
 import Data.Time.Clock
 import Data.Time.Calendar
+--import Data.Decimal
 
 
 taxRate :: Float
@@ -15,31 +16,41 @@ cardType x | head (digitize x) == 4 = "Visa"
 		   | head (digitize x) == 6 = "Discover"
 		   | otherwise = "Not Accepted"
 
-cDateTup = getCurrentTime >>= return.toGregorian.utctDay
+--cDateTup = getCurrentTime >>= return.toGregorian.utctDay
 --cDateTup2 <- fmap utctDay getCurrentTime
-cDateTup2 = fmap utctDay getCurrentTime
+--cDateTup2 = fmap utctDay getCurrentTime
 
 pastDate :: Integer -> Int -> Int -> IO Bool
 pastDate x y z = do 
 	dOne <- fmap utctDay getCurrentTime
 	return((fromGregorian x y z) < dOne)
 
+--calcTax :: Float -> Float
+--calcTax x = do
+--	d1 <- x * 0.1
+--	return fmap (roundTo 2) d1 
+
+--calcTax :: Float -> Float
+--calcTax x = do
+--	d1 <- x * 0.1
+--	return d1
+
 calcTax :: Float -> Float
-calcTax x = 0.0
+calcTax x = x * taxRate
 
 verifyCard :: Integer -> Integer -> Int -> Int -> IO Bool
 verifyCard cNum y m d = do
-	d1 = pastDate y m d
-	return ((cardType cNum) /= "Not Accepted") && d1)
+	d1 <- pastDate y m d
+	return (((cardType cNum) /= "Not Accepted") && not d1)
 
 checkOut :: Float -> IO ()
 checkOut num = do
 	putStrLn "Enter Cost: "
 	cost <- getLine
-	let amount = (read cost :: Float) 
+	let amount = (read cost :: Float)
 	if amount /= 0
-		then do putStrLn (show amount)
-				checkOut (amount + num)
+	  then do putStrLn (show amount)
+		  checkOut (amount + num)
 	else putStrLn (show (amount + (num + (calcTax (amount + num)))))
 
 
